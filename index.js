f75750bb5f171ca165065bc082c3b35fbcbb9ddc
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-09-16 22:22:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,19 @@ class Polygon {
 }
 class Triangle extends Polygon {
   get isValid () {
-    if (this.array[0] + this.array[1] === this.array[2] )
+    if (this.countSides === 3 && this.array[0] + this.array[1] > this.array[2] )
     return true;
     else
     return false;
   }
 }
+
+class Square extends Polygon {
+  get isValid () {
+    if (this.countSides === 4 && this.array[0] === this.array[1] === this.array[2] === this.array[3] )
+    return true;
+    else
+    return false;
+  }
+}
+
